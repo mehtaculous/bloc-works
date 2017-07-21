b15ea59454b362e89b185730eb604fc4439772fc
Sahil Mehta <samehta91@gmail.com>
2017-07-20 18:24:07 -0700

    Provide first class request and response objects and support advanced routing

--- lib/bloc_works.rb
+++ lib/bloc_works.rb
@@ -7,19 +7,12 @@ require "bloc_works/utility"
 module BlocWorks
   class Application
     def call(env)
-      if env['PATH_INFO'] != '/favicon.ico'
-        controller_class, action_name = controller_and_action(env)
-        if !controller_class.nil?
-          controller = controller_class.new(env)
-
-          if controller.respond_to?(action_name)
-            body = controller.send(action_name)
-            return [200, {'Content-Type' => 'text/html'}, [body]]
-          end
-        end
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
       end
 
-      return [404, {'Content-Type' => 'text/html'}, []]
+      rack_app = get_rack_app(env)
+      rack_app.call(env)
     end
   end
 end
--- lib/bloc_works/controller.rb
+++ lib/bloc_works/controller.rb
@@ -4,9 +4,52 @@ module BlocWorks
   class Controller
     def initialize(env)
       @env = env
+      @routing_params = {}
     end
 
-    def render(view, locals = {})
+    def dispatch(action, routing_params = {})
+      @routing_params = routing_params
+      text = self.send(action)
+      if has_response?
+        rack_response = get_response
+        [rack_response.status, rack_response.header, [rack_response.body].flatten]
+      else
+        [200, {'Content-Type' => 'text/html'}, [text].flatten]
+      end
+    end
+ 
+    def self.action(action, response = {})
+      proc { |env| self.new(env).dispatch(action, response) }
+    end
+
+    def request
+      @request ||= Rack::Request.new(@env)
+    end
+
+    def params
+      request.params.merge(@routing_params)
+    end
+
+    def response(text, status = 200, headers = {})
+      raise "Cannot respond multiple times" unless @response.nil?
+      @response = Rack::Response.new([text].flatten, status, headers)
+    end
+
+    def render(*args)
+      # if args.length is 0 or if it is one and args[0] is a hash, there was no view name passed
+      # if no view name is passed, assume "view" should be the action name
+      response(create_reponse_array(*args))
+    end
+
+    def get_response
+      @response
+    end
+
+    def has_response?
+      !@response.nil?
+    end
+
+    def create_reponse_array(view, locals = {})
       filename = File.join("app", "views", controller_dir, "#{view}.html.erb")
       template = File.read(filename)
       eruby = Erubis::Eruby.new(template)
